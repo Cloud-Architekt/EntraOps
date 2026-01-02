b740d39ef08340407a9565f428ac4d2b47f6656b
Thomas Naunheim <47817884+Cloud-Architekt@users.noreply.github.com>
2026-01-02 15:43:25 +0100

    Enhance Get-EntraOpsPrivilegedAppRoles to include delegated permissions

--- EntraOps/Public/PrivilegedAccess/Get-EntraOpsPrivilegedAppRoles.ps1
+++ EntraOps/Public/PrivilegedAccess/Get-EntraOpsPrivilegedAppRoles.ps1
@@ -1,15 +1,15 @@
 <#
 .SYNOPSIS
-    Get a list in schema of EntraOps with all service principals with exposed app roles in Entra ID.
+    Get a list in schema of EntraOps with all service principals with exposed app roles and delegated permissions in Entra ID.
 
 .DESCRIPTION
-    Get a list in schema of EntraOps with all service principals with exposed app roles in Entra ID.
+    Get a list in schema of EntraOps with all service principals with exposed app roles and delegated permissions in Entra ID.
 
 .PARAMETER TenantId
     Tenant ID of the Microsoft Entra ID tenant. Default is the current tenant ID.
 
 .EXAMPLE
-    List of all app roles from service principals in Entra ID.
+    List of all app roles and delegated permissions from service principals in Entra ID.
     Get-EntraOpsPrivilegedAppRoles
 #>
 
@@ -24,7 +24,8 @@ function Get-EntraOpsPrivilegedAppRoles {
     $ServicePrincipals = Invoke-EntraOpsMsGraphQuery -Uri "/v1.0/servicePrincipals"
 
     # Get all role assignments
-    $AppRoleAssignments = foreach ($ServicePrincipal in $ServicePrincipals) {
+    $AllAssignments = foreach ($ServicePrincipal in $ServicePrincipals) {
+        # Application Permissions
         $AppRoleAssignments = (Invoke-EntraOpsMsGraphQuery -Uri "/beta/servicePrincipals/$($ServicePrincipal.Id)/appRoleAssignments" -OutputType PSObject)
         if ($AppRoleAssignments.Id) {
             foreach ($AppRole in $AppRoleAssignments) {
@@ -47,19 +48,57 @@ function Get-EntraOpsPrivilegedAppRoles {
                 }
             }
         }
+
+        # Delegated Permissions
+        $DelegatedPermissions = (Invoke-EntraOpsMsGraphQuery -Uri "/beta/servicePrincipals/$($ServicePrincipal.Id)/oauth2PermissionGrants" -OutputType PSObject)
+        if ($DelegatedPermissions.Id) {
+            foreach ($Grant in $DelegatedPermissions) {
+                $Scopes = $Grant.scope -split " "
+                foreach ($Scope in $Scopes) {
+                    if (-not [string]::IsNullOrWhiteSpace($Scope)) {
+                        [pscustomobject]@{
+                            RoleAssignmentId              = $Grant.Id
+                            RoleAssignmentScopeId         = $Grant.resourceId
+                            RoleAssignmentScopeName       = $null
+                            RoleAssignmentType            = "Direct"
+                            PIMManagedRole                = $False
+                            PIMAssignmentType             = "Permanent"
+                            RoleDefinitionName            = $Scope
+                            RoleDefinitionId              = $null
+                            RoleType                      = "Delegated"
+                            RoleIsPrivileged              = ""
+                            Classification                = $null
+                            ObjectId                      = $Grant.clientId
+                            ObjectType                    = "serviceprincipal"
+                            TransitiveByObjectId          = $null
+                            TransitiveByObjectDisplayName = $null
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    # Get names for app roles
-    $AppRoleResourceIds = ($AppRoleAssignments | Select-Object -unique RoleAssignmentScopeId).RoleAssignmentScopeId
-    $AppRolesByResource = foreach ($AppRoleResourceId in $AppRoleResourceIds) {
-        Invoke-EntraOpsMsGraphQuery -Uri "/beta/servicePrincipals/$($AppRoleResourceId)?$select=appRoles,appDisplayName"
+    # Get names for app roles and delegated permissions
+    $ResourceIds = ($AllAssignments | Select-Object -unique RoleAssignmentScopeId).RoleAssignmentScopeId
+    $Resources = foreach ($ResourceId in $ResourceIds) {
+        Invoke-EntraOpsMsGraphQuery -Uri "/beta/servicePrincipals/$($ResourceId)?$select=appRoles,publishedPermissionScopes,appDisplayName"
     }
 
-    # Lookup for app role names for app role assignments
-    $AppRoleAssignments = foreach ($AppRoleAssignment in $AppRoleAssignments) {
-        $MatchedAppRole = $AppRolesByResource | where-object { $_.id -eq $AppRoleAssignment.RoleAssignmentScopeId } | select-object -ExpandProperty AppRoles | Where-Object { $_.id -eq $($AppRoleAssignment.RoleDefinitionId) }
-        $AppRoleAssignment.RoleDefinitionName = $MatchedAppRole.value
-        $AppRoleAssignment
+    # Lookup for app role names and delegated permission ids
+    $AppRoleAssignments = foreach ($Assignment in $AllAssignments) {
+        $Resource = $Resources | where-object { $_.id -eq $Assignment.RoleAssignmentScopeId }
+        
+        if ($Assignment.RoleType -eq "Application") {
+            $MatchedRole = $Resource | select-object -ExpandProperty AppRoles | Where-Object { $_.id -eq $($Assignment.RoleDefinitionId) }
+            $Assignment.RoleDefinitionName = $MatchedRole.value
+        } elseif ($Assignment.RoleType -eq "Delegated") {
+            $Assignment.RoleAssignmentScopeName = $Resource.appDisplayName
+            $MatchedScope = $Resource | select-object -ExpandProperty publishedPermissionScopes | Where-Object { $_.value -eq $($Assignment.RoleDefinitionName) }
+            $Assignment.RoleDefinitionId = $MatchedScope.id
+        }
+        $Assignment
     }
     $AppRoleAssignments
+    $AppRoleAssignments = $AppRoleAssignments | sort-object -property RoleAssignmentScopeName, RoleDefinitionName
 }
